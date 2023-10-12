41bc2fdab37216b6dae3a10c2aab2728021107b3
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-10-12 16:16:58 +0845

    가이드 작성

--- finance_tools/naver_finance.py
+++ finance_tools/naver_finance.py
@@ -2,6 +2,9 @@ import requests
 import pandas as pd
 
 def ko_etf_list() -> pd.DataFrame:
+    '''
+    [네이버페이 증권 ETF](https://finance.naver.com/sise/etf.naver) 목록을 데이터프레임으로 받아옴
+    '''
     DOMAIN = 'https://finance.naver.com'
     PATH = '/api/sise/etfItemList.nhn'
     URL = f'{DOMAIN}{PATH}'
@@ -10,23 +13,29 @@ def ko_etf_list() -> pd.DataFrame:
     df = pd.DataFrame(data)
     return df.set_index('itemcode')
 
-def not_contain(*kwds) -> str:
-    return ' and '.join([f'not itemname.str.contains("{kwd}")'
+def not_contain(*kwds, column_name='itemname') -> str:
+    '''
+    Column에 특정 키워드들이 포함되지 않은 것만 검색하는 쿼리 작성
+    '''
+    return ' and '.join([f'not {column_name}.str.contains("{kwd}")'
             for kwd in kwds])
 
 def filtered_ko_etf(
     exclude_kwds=('합성', '액티브', '레버리지', '2X',
                 '혼합', '금리', '단기', '3년', '배당', 'TR'),
     market_cap=1000) -> pd.DataFrame:
+    '''
+    특정 키워드를 제외하고, 시가총액으로 필터링한 ETF 데이터
+    '''
     ko_etf = ko_etf_list()
     ko_etf.dropna(inplace=True) # 상장 후 3개월 미만 제외
     if exclude_kwds:
         ko_etf.query(
-            not_contain(*exclude_kwds), inplace=True)
-    # 특정 키워드 포함 종목 제외
+            not_contain(*exclude_kwds, 'itemname'), inplace=True)
+    # 종목명 특정 키워드 포함 종목 제외
     ko_etf.query(f'marketSum >= {market_cap}', inplace=True)
     # 시총 {market_cap}억 미만 제외
-    ko_etf['group'] = ko_etf.itemname.str.replace('^\S* ', '', regex=True)
+    ko_etf['group'] = ko_etf['itemname'].str.replace('^\S* ', '', regex=True)
     # 중복 종목 제거 (시총 큰 것만 남김)
     return ko_etf.reset_index()\
         .groupby('group').first()\
