03fd73920094081c23bae88c1d9361b4e29038b5
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-10-12 13:27:35 +0845

    0.0.3 : filtered_ko_etf

--- finance_tools/naver_finance.py
+++ finance_tools/naver_finance.py
@@ -13,3 +13,22 @@ def ko_etf_list() -> pd.DataFrame:
 def not_contain(*kwds) -> str:
     return ' and '.join([f'not itemname.str.contains("{kwd}")'
             for kwd in kwds])
+
+def filtered_ko_etf(
+    exclude_kwds=('합성', '액티브', '레버리지', '2X',
+                '혼합', '금리', '단기', '3년', '배당', 'TR'),
+    market_cap=1000) -> pd.DataFrame:
+    ko_etf = ko_etf_list()
+    ko_etf.dropna(inplace=True) # 상장 후 3개월 미만 제외
+    if exclude_kwds:
+        ko_etf.query(
+            not_contain(*exclude_kwds), inplace=True)
+    # 특정 키워드 포함 종목 제외
+    ko_etf.query(f'marketSum >= {market_cap}', inplace=True)
+    # 시총 {market_cap}억 미만 제외
+    ko_etf['group'] = ko_etf.itemname.str.replace('^\S* ', '', regex=True)
+    # 중복 종목 제거 (시총 큰 것만 남김)
+    return ko_etf.reset_index()\
+        .groupby('group').first()\
+        .set_index('itemcode')\
+        .sort_values('marketSum', ascending=False)
