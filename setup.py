5ed81a3de21d8ab893fc545b72a3a9caa37f885a
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-10-12 16:29:03 +0845

    0.0.3

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
-from . import finance_tools
+
 setup(
     name='finance_tools',
-    version=finance_tools.__version__,
+    version='0.0.3',
     packages=find_packages(),
     install_requires=['requests', 'pandas'],
 )
